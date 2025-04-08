c74571175219ebef06902bc2346a073da9aff202
chowanza <luiscandialesfajardo@gmail.com>
2025-04-08 15:34:00 -0400

    55:02 Token created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,30 @@
 import Doctor from '../models/doctor.model.js';
+import bcrypt from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req, res) => {
     const {nombre, correo, clave} = req.body;
     
     try{
+
+        const claveHash = await bcrypt.hash(clave, 10)
         const newDoctor = new Doctor({
             nombre, 
             correo, 
-            clave
+            clave: claveHash,
         });
+
         const doctorSaved = await newDoctor.save();
-        res.json(doctorSaved)
+        const token = await createAccessToken({id: doctorSaved._id})
+        res.cookie('token', token)
+        res.json({
+            id: doctorSaved._id,
+            nombre: doctorSaved.nombre,
+            correo: doctorSaved.correo,
+            updatedAt: doctorSaved.updatedAt,
+        });
     } catch (error) {
-        console.log(error);
+        res.status(500).json({message: error.message});
     }
 
 }
